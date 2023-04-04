e1c1dce029410d10b1041d73589f0c64e958eac9
ChasmannJoel <67122039+ChasmannJoel@users.noreply.github.com>
2023-04-04 01:04:02 -0300

    primer commit

---
+++ buda-sushi/src/logo.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logoImg from "../src/assets/logo.jpg";
+import "./logo.css";
+
+const Logo = () => {
+  return (
+    <div className="logo-container">
+        <img className="rounded" src={logoImg} alt="Logo" />
+    </div>
+  )
+
+};
+
+export default Logo;
