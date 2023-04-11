2ac1e095abc306360e48f5c63b979d96b4d2849a
ChasmannJoel <67122039+ChasmannJoel@users.noreply.github.com>
2023-04-10 21:01:02 -0300

    elimino el nav

--- buda-sushi/src/Navbar.jsx
+++ buda-sushi/src/Navbar.jsx
@@ -5,9 +5,6 @@ function Navbar() {
   return (
     <nav>
       <ul>
-        <li><a href="#">Inicio</a></li>
-        <li><a href="#">Acerca de</a></li>
-        <li><a href="#">Contacto</a></li>
       </ul>
     </nav>
   );
